b0a7935210e4fdbd32f28f0d9124828a01efdba6
fernandoeduardo <fernandoeduardoqueiroz@gmail.com>
2020-03-30 17:05:38 -0300

    Project Structure

---
+++ ticket-inventory/src/main/java/br/com/microservices/ticket/inventory/TicketInventoryApplication.java
@@ -0,0 +1,13 @@
+package br.com.microservices.ticket.inventory;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TicketInventoryApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(TicketInventoryApplication.class, args);
+    }
+
+}
